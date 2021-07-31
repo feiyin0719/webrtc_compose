35a42129fc0c1c6862e2d023b791482377e22265
yinfei <yinfei@FX5C1N2.redmond.corp.microsoft.com>
2021-07-31 22:51:01 +0800

    使用buildSrc构建工程

--- buildSrc/src/main/java/com/iffly/webrtc_compose/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/com/iffly/webrtc_compose/buildsrc/Dependencies.kt
@@ -9,8 +9,11 @@ object Libs {
     const val koltinVersion = "1.5.10"
 
     object AndroidX {
-        const val coreKtx = "androidx.core:core-ktx:1.6.0-alpha03"
-
+        const val coreKtx = "androidx.core:core-ktx:1.3.2"
+        object Lifecycle {
+            const val runtimeKtx =
+                "androidx.lifecycle:lifecycle-runtime-ktx:2.3.1"
+        }
         object Compose {
             const val snapshot = ""
             const val version = "1.0.0-rc01"
@@ -25,6 +28,28 @@ object Libs {
             const val tooling = "androidx.compose.ui:ui-tooling:${version}"
             const val iconsExtended = "androidx.compose.material:material-icons-extended:$version"
             const val uiTest = "androidx.compose.ui:ui-test-junit4:$version"
+
+            object Activity {
+                const val activityCompose = "androidx.activity:activity-compose:1.3.0-rc01"
+            }
+
+        }
+
+        object Test {
+            private const val version = "1.3.0"
+            const val runner = "androidx.test:runner:$version"
+            const val rules = "androidx.test:rules:$version"
+            object Ext {
+                private const val version = "1.1.2"
+                const val junit = "androidx.test.ext:junit-ktx:$version"
+            }
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.2]3.0"
         }
     }
+
+    object JUnit {
+        private const val version = "4.13"
+        const val junit = "junit:junit:$version"
+    }
+
 }
