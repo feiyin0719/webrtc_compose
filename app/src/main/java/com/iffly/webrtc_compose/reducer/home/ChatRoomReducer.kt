0087bdb60c352ac201c8f468644c069b97995d44
iffly <582816317@qq.com>
2021-08-17 22:45:36 +0800

    fix  some error

--- app/src/main/java/com/iffly/webrtc_compose/reducer/home/ChatRoomReducer.kt
+++ app/src/main/java/com/iffly/webrtc_compose/reducer/home/ChatRoomReducer.kt
@@ -3,12 +3,9 @@ package com.iffly.webrtc_compose.reducer.home
 import com.iffly.compose.redux.Reducer
 import com.iffly.webrtc_compose.data.bean.ChatRoomItem
 import com.iffly.webrtc_compose.data.repo.net.UserRepo
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
 
 data class ChatRoomViewState(
     val loading: Boolean = true,
@@ -17,7 +14,7 @@ data class ChatRoomViewState(
 
 data class ChatRoomAction(val action: ChatRoomActionValue, val data: String) {
     enum class ChatRoomActionValue {
-        Refresh, Call
+        Refresh, Call, ChangeLoading
     }
 }
 
@@ -35,6 +32,9 @@ class ChatRoomReducer :
                 ChatRoomAction.ChatRoomActionValue.Call -> {
                     state.copy()
                 }
+                ChatRoomAction.ChatRoomActionValue.ChangeLoading -> {
+                    state.copy(loading = true)
+                }
                 ChatRoomAction.ChatRoomActionValue.Refresh -> {
 
                     try {
@@ -46,9 +46,6 @@ class ChatRoomReducer :
                     }
                 }
             }
-
-        }.flowOn(Dispatchers.IO).onStart {
-            emit(state.copy(loading = true))
         }
     }
 }
